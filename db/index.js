efc67450b773840a13f1cc0576012b0116f817d0
KenOasis <jtan17@mail.sfsu.edu>
2021-10-06 23:31:46 -0700

    test

--- db/index.js
+++ db/index.js
@@ -2,11 +2,9 @@ const pgp = require('pg-promise')();
 let config = {
   connectionString: process.env.DATABASE_URL,
   ssl: {
-    mode: 'verify-full',
-    rootcert: 'ca.pem'
+    rejectUnauthorized: false
   }
 }
-
 if (process.env.NODE_ENV === 'development') {
   config.connectionString = process.env.DATABASE_URL;
   config.ssl = false;
