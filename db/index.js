74c98e6c9e9b43135c32595a59ba4a251ac76a24
KenOasis <jtan17@mail.sfsu.edu>
2021-10-06 18:45:45 -0700

    m2 work locally

--- db/index.js
+++ db/index.js
@@ -1,5 +1,8 @@
 const pgp = require('pg-promise')();
 let connectionString = process.env.DATABASE_URL;
+if (process.env.NODE_ENV !== 'development') {
+  connectionString += "?sslmode=require";
+}
 
 const connection = pgp(connectionString);
 
