4b8a8bc277a248aadb6b1064d67059eae8260852
KenOasis <jtan17@mail.sfsu.edu>
2021-10-07 00:22:52 -0700

    test

--- db/index.js
+++ db/index.js
@@ -1,18 +1,5 @@
 const pgp = require('pg-promise')();
-const fs = require('fs');
-let config = {
-  connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false,
-    cert: fs.readFileSync('cert.pem'),
-    key: fs.readFileSync('key.pem')
-  }
-}
-if (process.env.NODE_ENV === 'development') {
-  config.connectionString = process.env.DATABASE_URL;
-  config.ssl = false;
-}
-const connection = pgp(config);
-// const connection = pgp(process.env.DATABASE_URL);
+
+const connection = pgp(process.env.DATABASE_URL + "?sslmode=no-verify");
 
 module.exports = connection;
