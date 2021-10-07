bee45546bea579269b5e337acd863d48de30e36d
KenOasis <jtan17@mail.sfsu.edu>
2021-10-07 00:20:14 -0700

    test

--- db/index.js
+++ db/index.js
@@ -1,15 +1,18 @@
 const pgp = require('pg-promise')();
-// let config = {
-//   connectionString: process.env.DATABASE_URL,
-//   ssl: {
-//     rejectUnauthorized: false,
-//   }
-// }
-// if (process.env.NODE_ENV === 'development') {
-//   config.connectionString = process.env.DATABASE_URL;
-//   config.ssl = false;
-// }
-// const connection = pgp(config);
-const connection = pgp(process.env.DATABASE_URL);
+const fs = require('fs');
+let config = {
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+    cert: fs.readFileSync('cert.pem'),
+    key: fs.readFileSync('key.pem')
+  }
+}
+if (process.env.NODE_ENV === 'development') {
+  config.connectionString = process.env.DATABASE_URL;
+  config.ssl = false;
+}
+const connection = pgp(config);
+// const connection = pgp(process.env.DATABASE_URL);
 
 module.exports = connection;
