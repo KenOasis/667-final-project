a6cedba141322c87641a8e8e4a0695b34e29fe11
KenOasis <jtan17@mail.sfsu.edu>
2021-10-07 00:23:31 -0700

    test

--- db/index.js
+++ db/index.js
@@ -1,5 +1,5 @@
 const pgp = require('pg-promise')();
 
-const connection = pgp(process.env.DATABASE_URL + "?sslmode=no-verify");
+const connection = pgp(process.env.DATABASE_URL + "?ssl=no-verify");
 
 module.exports = connection;
