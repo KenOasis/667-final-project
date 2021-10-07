c7772f38fdaedfe1d0542e1388f73e7c5e35e31c
KenOasis <jtan17@mail.sfsu.edu>
2021-10-06 17:41:16 -0700

    fixed heroku progresql ssl error 28000

--- db/index.js
+++ db/index.js
@@ -1,4 +1,13 @@
 const pgp = require('pg-promise')();
-const connection = pgp(process.env.DATABASE_URL);
+let ssl = true;
+if (process.env.NODE_ENV === 'development') {
+  ssl = false;
+}
+const config = {
+  connectionString: process.env.DATABASE_URL,
+  max: 30,
+  ssl: ssl
+}
+const connection = pgp(config);
 
 module.exports = connection;
