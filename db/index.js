0e33fa2f6e9766517599507aaec07395dcd327bb
KenOasis <jtan17@mail.sfsu.edu>
2021-10-07 00:27:39 -0700

    M2 Final version

--- db/index.js
+++ db/index.js
@@ -1,5 +1,7 @@
 const pgp = require('pg-promise')();
 
+// Heroku Postgresql DO NOT SUPPORT SSL with free tier
+// ADD Config Vars "PGSSLMODE:no-verify" at the app setting
 const connection = pgp(process.env.DATABASE_URL);
 
 module.exports = connection;
