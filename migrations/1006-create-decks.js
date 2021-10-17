b4976cb83b70178f9dd79a0bf30b47aa983d27c0
KenOasis <jtan17@mail.sfsu.edu>
2021-10-16 20:15:03 -0700

    fixed bugs

--- migrations/1006-create-decks.js
+++ migrations/1006-create-decks.js
@@ -37,6 +37,6 @@ module.exports = {
     );
   },
   down: (queryInterface, Sequelize) => {
-    return queryInterface.dropTable('game_users');
+    return queryInterface.dropTable('decks');
   }
 }
