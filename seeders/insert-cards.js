b6a3566bb4837935a1b61b3ae40561da8fa5b0ac
KenOasis <jtan17@mail.sfsu.edu>
2021-10-17 16:53:24 -0700

    fix seeders

--- seeders/insert-cards.js
+++ seeders/insert-cards.js
@@ -4,6 +4,7 @@ const cardsBulk = require('../db/cards_bulk');
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
+    queryInterface.bulkDelete('cards', null, {});
     return queryInterface.bulkInsert('cards', cardsBulk);
   },
   down: (queryInterface, Sequelize) => {
