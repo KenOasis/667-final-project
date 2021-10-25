67a533af5012e74058bbcf340263a759877be4ab
Lucas Carvajal <lcs.carvajal@gmail.com>
2021-10-25 11:46:53 -0700

    redirect after successful registering

--- controllers/users.js
+++ controllers/users.js
@@ -4,43 +4,18 @@ const Users = db['users'];
 
 
 exports.signUp = async (req, res, next) => {
-  console.log("===========================#===============##==============#==========")
-  console.log(req.body)
-  console.log(req.body.username)
-  console.log("===========================#===============##==============#==========")
   const username = req.body.username;
   const email = req.body.email;
   const password = req.body.password;
-  // const username = "new user";
-  // const email = "test@test.com";
-  // const password = "Nu123456789";
   Users.create({
     username: username,
     email: email,
     password: password,
-  }).then( results => res.status(200).json(
-    {
-      id: results.id,
-      username: results.username,
-      email: results.email
-    }
-  )).catch(error => {
+  }).then( results => 
+    res.redirect('/login')
+  ).catch(error => {
     console.log(error);
     res.json({error: error});
   });
 }
 
-exports.signUpPut = async (req, res, next) => {
-  console.log("PUT REqUEST")
-  this.signUp(req, res, next)
-}
-
-exports.signUpPost = async (req, res, next) => {
-  console.log("POST REqUEST")
-  this.signUp(req, res, next)
-}
-
-exports.signUpGet = async (req, res, next) => {
-  console.log("GET REqUEST")
-  this.signUp(req, res, next)
-}
--- routes/users.js
+++ routes/users.js
@@ -2,8 +2,6 @@ const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/users');
 
-// router.get('/signup', userController.signUpGet);
-router.post('/signup', userController.signUpPost)
-router.put('/signup', userController.signUpPut)
+router.post('/signup', userController.signUp)
 
 exports.routes = router;
