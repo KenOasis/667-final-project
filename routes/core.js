a1cc212f7b6d9943038103883932768865144f35
KenOasis <jtan17@mail.sfsu.edu>
2021-10-26 10:37:22 -0700

    add /tests/show_random_cards

--- routes/core.js
+++ routes/core.js
@@ -4,6 +4,7 @@ const router = express.Router()
 const gameTestController = require('../controllers/core/test');
 
 router.get('/show_all_cards', gameTestController.showAllCards);
+router.get('/show_random_cards', gameTestController.showRandomCards);
 router.get('/test_game', gameTestController.testGame);
 router.get('/test_gu', gameTestController.testGameUsers);
 router.get('/test_gc', gameTestController.testGameCards);
