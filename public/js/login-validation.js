2423108059c8b9e489f78f0ea58394a6cd26431b
KenOasis <jtan17@mail.sfsu.edu>
2021-11-02 18:27:17 -0700

    finish validation of authorization

---
+++ public/js/login-validation.js
@@ -0,0 +1,43 @@
+const liveToast = document.getElementById('loginToast');
+
+const onSubmitLogin = () => {
+  const url = "http://" + location.host + "/user/login";
+  const body = {
+    username: document.getElementById("username").value,
+    password: document.getElementById("password").value
+  };
+  fetch(url, {
+    method: "POST",
+    body: JSON.stringify(body),
+    headers: new Headers({
+      "content-type": "application/json",
+    }),
+  })
+    .then((response) => response.json())
+    .then((results) => {
+      if (results.errors) {
+        if(liveToast) {
+          let errors = results.errors;
+          let errorMsg = `<b class="text-danger">{${errors[0].param}} : ${errors[0].msg}</b>`;
+          document.getElementById('errors_message').innerHTML = errorMsg
+          var toast = new bootstrap.Toast(liveToast);
+          toast.show();
+        }
+      } else {
+        // success 
+        if (results.url) {
+          window.location.href = results.url;
+        }
+      }
+    })
+    .catch((err) => {
+      if(liveToast) {
+        let errorMsg = `<b class="text-danger">${err.name} : ${err.message}/b>`;
+        document.getElementById('errors_message').innerHTML = errorMsg
+        var toast = new bootstrap.Toast(liveToast);
+        toast.show()
+      } else {
+        console.log(err);
+      }
+    });
+}
--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -1,40 +1,15 @@
 const express = require('express');
 const router = express.Router();
 const userController = require('../../controllers/user-controller');
-const { check, validationResult } = require('express-validator');
-const passwordRegex = /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/;
-router.post('/signup', userController.signUp);
+const backendValidator = require('../../middleware/backend-validator');
+
+router.post('/signup', backendValidator.signupValidation, userController.signUp);
 
 router.post('/login', userController.login);
 
 router.get('/logout', userController.logout);
-router.post('/change_password', 
-    check('current_password').matches(passwordRegex).withMessage("Must be length >= 8, contain number, letter and special character"),
-    check('new_password').matches(passwordRegex).withMessage("Must be length >= 8, contain number, letter and special character"),
-    check('new_password').custom((value, {req}) => {
-      if (value !== req.body.confirm_password) {
-        throw new Error('Password confirmation is not match with new password');
-      }
-      return true;
-    }),
-    check('confirm_password').matches(passwordRegex).withMessage("Must be length >= 8, contain number, letter and special character"),
-    check('confirm_password').custom((value, {req}) => {
-      if (value !== req.body.new_password) {
-        throw new Error('Password confirmation is not match with new password');
-      }
-      return true;
-    }),
-  (req, res, next) => {
-    
-    var err = validationResult(req);
-    console.log(err)
-    if (!err.isEmpty()) {
-       res.status(400).json(err)
-    } else {
-        next()
-    }
-  },
-  userController.changePassword);
+
+router.post('/change_password', backendValidator.changePWValidation, userController.changePassword);
 
 router.get('/profile', userController.getProfile);
 exports.routes = router;
